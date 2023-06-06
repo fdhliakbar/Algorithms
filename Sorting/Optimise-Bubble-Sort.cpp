13b48f38f14557100fd486cea97c97c51bd3f55b
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-06-06 11:15:34 +0700

    Add files via upload

---
+++ Sorting/Optimise-Bubble-Sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int array[], int size) {
+    for (int i = 0; i < (size-1); ++i) {
+    int count = 0;
+    for (int j = 0; j < (size-i-1); ++j) {
+        if (array[j] > array[j + 1]) {
+        int temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        count = 1;
+        }
+    }
+    if (count == 0) break;
+    cout << "Tahap " << i+1 << " : ";
+    for(int k = 0; k < size; k++){
+        cout << array[k] << " ";
+    }
+    cout << '\n';
+    }
+    cout<<'\n';
+}
+
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; ++i) {
+    cout << array[i] << " ";
+    }
+    cout << "\n\n";
+}
+
+int main() {
+    int data[5] = {-2, 45, 0, 11, -9};
+    int size = sizeof(data) / sizeof(data[0]);
+
+    cout << "Before: ";
+    printArray(data, size);
+
+    bubbleSort(data, size);
+
+    cout << "After: ";
+    printArray(data, size);
+    return 0;
+}
