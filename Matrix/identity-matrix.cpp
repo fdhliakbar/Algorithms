d887e26002650ab16fda978ac767e57c171539cb
fdhliakbar <fadhliakbar125@gmail.com>
2023-06-30 11:36:58 +0700

    fix identity matrix

--- Matrix/identity-matrix.cpp
+++ Matrix/identity-matrix.cpp
@@ -1,57 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
-void matrixElementInput(int matrix[][10], int row, int col){
-    std::cout<<"Masukkan jumlah baris : ";
-    std::cin>>row;
+const int MAX = 100;
 
-    std::cout<<"Masukkan jumlah kolom : ";
-    std::cin>>col;
-
-    std::cout<<"\nMasukkan elemen-elemen matrix : \n";
-
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j < col; j++){
-            std::cout<<"Baris ke-"<<(i+1)<<" kolom ke-"<<(j+1)<<" : ";
-            std::cin>>matrix[i][j];
+bool isIdentity(int mat[][MAX], int N){
+    for (int row = 0; row < N; row++)
+    {
+        for (int col = 0; col < N; col++)
+        {
+            if (row == col && mat[row][col] != 1)
+                return false;
+            else if (row != col && mat[row][col] != 0)
+                return false;
         }
     }
-}
-
-void sorting(int matrix[][10], int row, int col){
-}
-
-void printMatrix(int matrix[][10], int row, int col){
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            std::cout << matrix[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
+    return true;
 }
 
 int main(){
-    int matrix[10][10];
-    int row, col;
-    int userInput;
+    int N = 4;
+    int mat[][MAX] = {
+        {1, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 1}
+        };
 
-    std::cout<<"+++++ PILIHAN +++++\n\n";
-    std::cout<<"1.Input\n2.Sorting\n3.Cetak\n4.Operasi\n";
-    std::cout<<"Pilihan anda : ";
-    std::cin>>userInput;
-
-    switch (userInput) {
-    case1:
-        matrixElementInput(matrix,row,col);
-        break;
-    case2:
-        printMatrix(matrix,row,col);
-        break;
-    case3:
-        sorting(matrix,row,col);
-        break;
-    default:
-        break;
-    }
-
+    if (isIdentity(mat, N))
+        cout << "Matrix is identity";
+    else
+       cout << "Matrix is not an identity";
     return 0;
 }
