2dc158e57e48c658b369198c5ebb1baf9b1ca06e
fdhliakbar <fadhliakbar125@gmail.com>
2023-07-01 22:54:43 +0700

    upload file string pak ardi

--- Search/string_ardiansyah.cpp
+++ Search/string_ardiansyah.cpp
@@ -1,33 +1,52 @@
 #include <iostream>
 using namespace std;
 
-int getStringLenght(string characters){
-
-}
-
-void delString(int startIndex, int lastIndex, string name){
-    string deletedString = "";
-
-    for (int kar = startIndex; kar < lastIndex; kar++){
-        deletedString += name[kar];
+int getStringLength(string strings){
+    int length = 0;
+    while (strings[length] != '\0'){
+        length = length + 1;
     }
-
-    cout << deletedString;
+    return length;
 }
 
-void findSubString(string name, string keyword){
-    int slices[5];
+string delString(string lastSlice){
+    int lastSliceLength = getStringLength(lastSlice);
+    string deletedString = "";
+    for (int kar = 1; kar < lastSliceLength; kar++)
+    {
+        deletedString = deletedString + lastSlice[kar];
+    }
+    return deletedString;
+}
+
+void findSubstring(string nama, string keyword){
+    string slices[5];
+    int indexes[2];
     bool found = false;
+    string lastSlice = "";
+    int strLength = getStringLength(nama);
+    int keywordLength = getStringLength(keyword);
+    
+    for (int i =0; i < strLength; i++){
+        lastSlice = lastSlice + nama[i];
+        int lastSliceLength = getStringLength(lastSlice);
+
+        if (lastSliceLength == keywordLength && lastSlice != keyword){
+            lastSlice = delString(lastSlice);
+            slices[i] = lastSlice;
+        }
+        if (lastSliceLength == keywordLength && lastSlice == keyword){
+            int startIndex = (i - keywordLength ) + 1;
+            int lastIndex = i;
+            cout << "keyword" << " " << keyword << " ditemukan pada indeks " << startIndex << " - " << lastIndex << " dari string " << nama;
+            break;
+        }
+    }
+    
 }
 
 int main(){
-    string names = "Ardiansyah";
+    string name = "Ardiansyah";
     string keyword = "dian";
-    int startIndex = 6;
-    int lastIndex = 9;
-
-    //delString(startIndex, lastIndex, names);
-    getStringLenght(keyword);
-
-    return 0;
-}
+    findSubstring(name, keyword);
+} 
