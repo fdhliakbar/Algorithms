a2bc317a5217c75a600e32e533fab2456ae38362
fdhliakbar <fadhliakbar125@gmail.com>
2023-07-04 23:29:14 +0700

    tambahkan fungsi printMatrix di file UAS_Alpro

--- Studi/UAS_Alpro.cpp
+++ Studi/UAS_Alpro.cpp
@@ -1,20 +1,44 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void findString(){
+void summation(){
 
 }
 
-void findSubString(){
-    
+void sorting(){
+    int temp;
+
+    for (int row = 0; row < 3; row++){
+        for (int col = 1; col < 3; col++) {
+            if (row > col) {
+                temp = row;
+            }
+        }
+    }
 }
 
-void deleteString(){
-    
+void printMatrix(int nim[][3]){
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+            cout << nim[row][col] << " ";
+        }
+        cout << '\n';
+    }
 }
-
 
 int main(){
+    int nim[3][3] = {
+        {197, 180, 190},
+        {120, 124, 105},
+        {200, 198, 253},
+    };
 
+    string mhsName[3][3] = {
+        {"fadhli", "bota", "riko"},
+        {"shiro", "kiko", "sophie"},
+        {"wayne", "arthur", "monalisa"},
+    };
+
+    printMatrix(nim);
     return 0;
 }
