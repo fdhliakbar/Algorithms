b244abdafc1930fd01391eef3cf24943c1561071
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-07 20:46:23 +0700

    upload add-text.js

---
+++ Data Structure/String/add-text.js
@@ -0,0 +1,22 @@
+/*
+    arrays
+matrix
+strings
+linked
+stack
+queue
+tree
+heap
+hashing
+graph
+map
+*/
+
+function stringSummation(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+let firstName = "fadhli";
+let lastName = "akbar";
+
+console.log("Your name is", stringSummation(firstName, lastName));
