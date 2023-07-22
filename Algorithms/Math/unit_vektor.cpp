e9f8a5a56c5feaf1c19026c64975b86f25d7a81b
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-22 10:34:45 +0700

    upload unit vektor file

---
+++ Algorithms/Math/unit_vektor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// vektor AB = y2-x1 and y1 - x2
+// panjang vektor AB = (x2-x1)2 + (y2-y1)2
+
+int calculateTheUnitVector(int a, int b) {
+    /*
+        Vektor yang pangjanganya satu satuan
+        e =
+    */
+   return a * b;
+}
+
+
+int main()
+{
+    int a = 4 + 3;
+    int lengthVektor = ((4*4) + (3*3));
+    std::cout << lengthVektor;
+    return 0;
+}
