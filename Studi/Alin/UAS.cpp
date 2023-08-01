6bfa1fe181a24af0606d711d9894eda07a545cfb
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-08-01 22:57:06 +0700

    upload uas file

---
+++ Studi/Alin/UAS.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int convertToCosinus(const short int alpha) {
+
+}
+
+int translationsRotaions(short int A[], short int p[]) {
+    short int result;
+    for (int i = 0; i < 2; i++) {
+        if (i == 0) {
+            A[0] -= p[0];
+            A[1] -= p[1];
+        }
+        
+    }
+}
+
+int main(){
+    short int A[2] = {3, 4};
+    short int centerPoint[2] = {2, -3};
+    short int alpha = 90;
+    return 0;
+}
