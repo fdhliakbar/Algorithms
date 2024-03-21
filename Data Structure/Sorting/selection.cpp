57ec99e34a30e6dab7a2a06e72bfe332da61e465
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2024-03-21 19:21:26 +0700

    Fix: selection sort

--- Data Structure/Sorting/selection.cpp
+++ Data Structure/Sorting/selection.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int arr[5] = {4,2,3,1,5};
+void selectionSort(int arr[], int N) {
+
+    for (int i = 0; i < N - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < N; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+
+int main() {
+    int arr[5] = {2, 3, 1, 5, 4};
     int arrSize = sizeof(arr) / sizeof(arr[0]);
 
-    for (int i = 0; i < arrSize; i++){
+    cout << "Before sorting: ";
+    for (int i = 0; i < arrSize; i++) {
         cout << arr[i] << " ";
     }
-    cout << "\n\nAfter: ";
-    for (int i = 0; i < arrSize; i++){
-        for (int j = 0; j < arrSize; j++) {
-            cout << endl;
-        }
+
+    selectionSort(arr, arrSize);
+
+    cout << "\nAfter sorting: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << arr[i] << " ";
     }
+
+    return 0;
 }
